934f591243ce868076f3f090aab50fcf9dfc4672
BiggillyBoyMan <lucaslow57@gmail.com>
2025-08-21 18:50:45 +0800

    this is the second version of the app

--- src/main/java/Nami.java
+++ src/main/java/Nami.java
@@ -4,14 +4,27 @@ public class Nami {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("____________________________________________________________\n");
         System.out.println(" Hello! I'm Nami\n");
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________________________________________\n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println(" Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
 
+        while (true) {
+            String input = sc.nextLine();
+
+            if(input.equalsIgnoreCase( "bye")) {
+                System.out.println("____________________________________________________________\n");
+                System.out.println(" Bye. Hope to see you again soon!\n");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
