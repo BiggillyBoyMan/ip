e07b9348a1028f07a97dd079f5efc6b7b6dcbbd6
BiggillyBoyMan <lucaslow57@gmail.com>
2025-08-21 18:47:35 +0800

    level 0

---
+++ src/main/java/Nami.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Nami {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________\n");
+        System.out.println(" Hello! I'm Nami\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________\n");
+        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________________________________");
+
+    }
+}
